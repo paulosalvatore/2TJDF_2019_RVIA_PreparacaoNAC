cb17c3b06c703b63228cbfc8c79b390be63e7918
Paulo Salvatore <paulo@salvatore.tech>
2019-10-08 09:20:06 -0300

    Added MaquinaEstado properties

--- Assets/MaquinaEstado.cs
+++ Assets/MaquinaEstado.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class MaquinaEstado : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public enum Estados
+    {
+        Esperar,
+        Patrulhar,
+        Perseguir
+    }
+
+    private Estados estadoAtual;
+
+    // Esperar
+    public float tempoEsperar = 2f;
+    private float tempoEsperando;
+
+    // Patrulhar
+    public Transform waypoint1;
+    public Transform waypoint2;
+    public Transform waypoint3;
+    private Transform waypointAtual;
+    public float distanciaMinimaWaypoint = 1.5f;
+    private float distanciaWaypointAtual;
+
+    // Perseguir
+    public float distanciaMinimaPerserguicao = 5f;
+    private float distanciaAtualPlayer;
+    private GameObject player;
+
     private void Start()
     {
     }
 
-    // Update is called once per frame
     private void Update()
     {
     }
