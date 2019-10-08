af5580b65b0323504d39c541f55cca988f8bedf1
Paulo Salvatore <paulo@salvatore.tech>
2019-10-08 08:38:46 -0300

    SpawnKamikaze

--- Assets/SpawnKamikaze.cs
+++ Assets/SpawnKamikaze.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class SpawnKamikaze : MonoBehaviour
 {
+    public GameObject kamikazePrefab;
+
+    public float delay = 2.5f;
+
     private void Start()
     {
+        InvokeRepeating("Spawn", delay, delay);
     }
 
-    private void Update()
+    private void Spawn()
     {
+        var instancia = Instantiate(kamikazePrefab);
+
+        var posicao = transform.position;
+        posicao.y = instancia.transform.position.y;
+
+        instancia.transform.position = posicao;
     }
 }
