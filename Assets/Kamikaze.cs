e5177ef0b1ddb51c7102db5d421709f8ca3b9f3b
Paulo Salvatore <paulo@salvatore.tech>
2019-10-08 08:34:07 -0300

    Initial commit

---
+++ Assets/Kamikaze.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamikaze : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
---
+++ Assets/SpawnKamikaze.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKamikaze : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
