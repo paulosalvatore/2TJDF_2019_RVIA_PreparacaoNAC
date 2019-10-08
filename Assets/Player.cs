360f1484470070a5e77f4d9e0330b2f8d75f133d
Paulo Salvatore <paulo@salvatore.tech>
2019-10-08 09:10:51 -0300

    Added Player

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocidade = 1f;
+
+    public float velocidadeRotacao = 100f;
+
+    private void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        transform.Translate(
+            Vector3.forward * v * velocidade * Time.deltaTime
+        );
+
+        transform.Rotate(
+            Vector3.up * h * velocidadeRotacao * Time.deltaTime
+        );
+    }
+}
